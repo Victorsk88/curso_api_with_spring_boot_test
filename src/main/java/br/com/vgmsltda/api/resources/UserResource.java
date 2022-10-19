39101d4d977a3fdeff492577d983055a3a1681f1
Victor Gabriel <victor_gabrielms@yahoo.com.br>
2022-10-19 11:52:03 -0300

    impruving code and add findAll with method

--- src/main/java/br/com/vgmsltda/api/resources/UserResource.java
+++ src/main/java/br/com/vgmsltda/api/resources/UserResource.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -21,5 +24,15 @@ public class UserResource {
     @GetMapping(value ="/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
         return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+//        List<UserDTO> listDTO =
+//                service.findAll().stream().map(x -> mapper.map(x,UserDTO.class)).collect(Collectors.toList());
+//        return ResponseEntity.ok().body(listDTO);
+
+        return ResponseEntity.ok().body(service.findAll()
+                .stream().map(x -> mapper.map(x,UserDTO.class)).collect(Collectors.toList()));
     }
 }
--- src/main/java/br/com/vgmsltda/api/services/UserService.java
+++ src/main/java/br/com/vgmsltda/api/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.vgmsltda.api.services;
 
 import br.com.vgmsltda.api.domain.Users;
 
+import java.util.List;
+
 
 public interface UserService {
     Users findById(Integer id);
+
+    List<Users> findAll();
 }
