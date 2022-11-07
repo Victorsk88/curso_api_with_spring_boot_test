fd4ea65f7c3bc05a017130e935bc50c3de836a33
Victor Gabriel <victor_gabrielms@yahoo.com.br>
2022-11-07 15:41:09 -0300

    test dataIntegrityViolationException method

--- src/test/java/br/com/vgmsltda/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/vgmsltda/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,4 +1,5 @@
 package br.com.vgmsltda.api.resources.exceptions;
+import br.com.vgmsltda.api.services.exceptions.DataIntegrityViolationException;
 import br.com.vgmsltda.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,12 +8,16 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResourceExceptionHandlerTest {
 
 
     public static final String OBJECTNOTFOUND = "Objeto nao encontrado de Id : ";
+    public static final String EMAIL_JÁ_CADASTRADO = "Email já cadastrado";
 
     @InjectMocks
     private ResourceExceptionHandler handler;
@@ -29,6 +34,8 @@ class ResourceExceptionHandlerTest {
                 new ObjectNotFoundException(OBJECTNOTFOUND),
                 new MockHttpServletRequest());
 
+
+
         assertNotNull(response);
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.NOT_FOUND ,response.getStatusCode());
@@ -36,9 +43,27 @@ class ResourceExceptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals(OBJECTNOTFOUND, response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+
+//        assertEquals("/user/2", response.getBody().getPath());
+        assertEquals(LocalDateTime.now(), response.getBody().getTimeStamp());
     }
 
     @Test
     void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = handler.dataIntegrityViolationException(
+                new DataIntegrityViolationException(EMAIL_JÁ_CADASTRADO)
+                , new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(404 , response.getBody().getStatus());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class , response.getBody().getClass());
+        assertEquals(EMAIL_JÁ_CADASTRADO, response.getBody().getError());
+
+
+
+
     }
 }
